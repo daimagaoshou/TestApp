c1c89286dd39c5e0698b6c92b17cffeae95ebf97
daimagaoshou <296449668@qq.com>
2022-04-01 22:44:51 +0800

    切换到dev

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var tv = findViewById<TextView>(R.id.tv)
         tv.text = "asdasdsda"
+        Log.e("info", "sadsadsad")
     }
 }
